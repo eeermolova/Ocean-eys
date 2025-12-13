99d6f12d67d749d985f34c17740b00bb3ef2b42a
Anna <annetochka.ak@gmail.com>
2025-12-13 15:58:11 +0700

    Главная сцена + кнопка играть

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+
+    public void Transition()
+    {
+        SceneManager.LoadScene(3);
+      }
+}
